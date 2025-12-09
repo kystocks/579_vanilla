a685b6186ea5cbf7ee4b3c25dd3426ebf43b607e
kystocks <kstocks@umich.edu>
2025-12-09 10:29:39 -0500

    Log to the console when clicked (satisfies the "interactive" requirement) Change the button text to "Clicked!" visual feedback

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ if (typeof module !== 'undefined') {
   }
 }
 
+const addNumbers = (first, second) => {
+  return first + second;
+}
+
+// Add this interactive functionality for the button
+document.querySelector('button').addEventListener('click', () => {
+  console.log('Button clicked!');
+  // Change the button text to show it worked
+  document.querySelector('button').textContent = 'Clicked!';
+});
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    addNumbers
+  }
+}

841de82c2db25674562972537fb380484808464c
kystocks <kstocks@umich.edu>
2025-12-09 10:33:52 -0500

    corrected accidental code duplications

--- js/script.js
+++ js/script.js
@@ -2,16 +2,6 @@ const addNumbers = (first, second) => {
   return first + second;
 }
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
-
-const addNumbers = (first, second) => {
-  return first + second;
-}
-
 // Add this interactive functionality for the button
 document.querySelector('button').addEventListener('click', () => {
   console.log('Button clicked!');
